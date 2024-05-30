a11a74727d816096c34569d975d356edd5a8de89
Nicolas Duarte <nicollorente2002@gmail.com>
2024-05-29 23:57:19 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import styled from 'styled-components';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +10,7 @@ export default defineConfig({
         '@fortawesome/react-fontawesome',
         '@fortawesome/free-solid-svg-icons',
         '@fortawesome/free-brands-svg-icons',
-        'styled-components'  
+        'styled-components'
       ],
     },
   },
