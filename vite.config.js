1ce900a90d501db4dd49d3001f31de7eeb0e8cbe
Nicolas Duarte <nicollorente2002@gmail.com>
2024-05-29 23:24:17 -0300

    agregado en external styled-components

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     rollupOptions: {
       external: [
         "@fortawesome/free-brands-svg-icons",
-        "@fortawesome/free-solid-svg-icons"
+        "@fortawesome/free-solid-svg-icons",
+        "styled-components"
       ],
     },
   },
