5ad8e1984befe5731f6e8cadd779611a84f24835
Nicolas Duarte <nicollorente2002@gmail.com>
2024-05-29 23:18:05 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-      rollupOptions: {
-        external: ["@fortawesome/free-brands-svg-icons"],
-      },
+  build: {
+    rollupOptions: {
+      external: [
+        "@fortawesome/free-brands-svg-icons",
+        "@fortawesome/free-solid-svg-icons"
+      ],
     },
+  },
 })
