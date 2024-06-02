15c5aa6f7fd50464323d9861275b70ad93337764
Nicolas Duarte <nicollorente2002@gmail.com>
2024-06-02 18:59:22 -0300

    cambios en vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Hamburger from 'hamburger-react';
 
 export default defineConfig({
-  plugins: [react(),],
-  build: {
-    rollupOptions: {
-      external: ['hamburger-react']
-    }
-  }
+  plugins: [react()]
   
 });
