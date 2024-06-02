20474f78ae3d2e0b9c0b9a69a33463870edf59f6
Nicolas Duarte <nicollorente2002@gmail.com>
2024-06-02 15:22:54 -0300

    Cambios en cv

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { Divide as Hamburger } from 'hamburger-react'
 // import { useSelector } from "react-redux";
 // import { useDispatch } from "react-redux";
 // import HamburgerIcon from "./HamburgerIcon";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
-
   const handleClick = () => {
     setClicked(!clicked);
   };
@@ -25,9 +25,9 @@ const Navbar = () => {
            <li><a href="#contacto"  onClick={handleClick}>Contacto</a></li>
         </ul>
       </nav>
-      {/* <div className="icon-bar">
-        <HamburgerIcon clicked={clicked} handleClick={handleClick} />
-      </div> */}
+       <div className="icon-bar" >
+   <Hamburger  clicked={clicked} handleClick={handleClick} />
+      </div> 
     </header>
   );
 };
