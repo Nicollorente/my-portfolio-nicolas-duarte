065502caec40608a694bd6436c030d0c275fd554
Nicolas Duarte <nicollorente2002@gmail.com>
2024-06-02 21:40:22 -0300

    cambios en el css responsive

--- src/Components/Inicio.jsx
+++ src/Components/Inicio.jsx
@@ -1,24 +1,34 @@
 const Inicio = () => {
   return (
-    <section id="inicio" className="home" >
+    <section id="inicio" className="home">
       <div className="content">
         <div className="name">
           HOLA! SOY <span>NICOLÁS DUARTE</span>
         </div>
 
-        <div className="front">
-
-          PROGRAMADOR WEB FRONT END
-        </div>
+        <div className="front">PROGRAMADOR WEB FRONT END</div>
         <br />
         <br />
         <div className="des">
+          Desarrollador Front-End con experiencia en proyectospersonales y
+          académicos, especializado en React y tecnologías web modernas.
+          Apasionado por crear experiencias de usuario atractivas y eficientes.
+          Busco oportunidades para aplicar mis habilidades en un entorno
+          profesional.
+          <br /> <br />
           Tras el tiempo que me tomé de seguir mejorando para adquirir mas
           experiencia, he tomado la decisión de reorientar mi carrera
           profesional con el ánimo de continuar aprendiendo y, también, aportar
           toda mi experiencia adquirida.
         </div>
-        <a className="cv" href="/CV Nicolás Iñaki Duarte Front-End.pdf"  target="_blank" rel="noopener noreferrer">Mi Curriculum Vitae</a>
+        <a
+          className="cv"
+          href="/CV Nicolás Iñaki Duarte Front-End.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi Curriculum Vitae
+        </a>
       </div>
 
       <div className="avatar">
